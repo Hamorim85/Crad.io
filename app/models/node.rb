4298fbe369586c6c66987c4fb5b256829d884652
Caio Andrade <caioertai@gmail.com>
2018-04-12 07:51:44 -0300

    Added igid validations to Nodes as it is VERY important to the researcher that they have them

--- app/models/node.rb
+++ app/models/node.rb
@@ -3,5 +3,5 @@ class Node < ApplicationRecord
   has_many :followers, through: :follower_nodes
   has_many :node_categories
   has_many :categories, through: :node_categories
-  validates :name, uniqueness: true, presence: true
+  validates :name, :igid, uniqueness: true, presence: true
 end
