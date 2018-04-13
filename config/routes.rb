1f6e91ad181faccc8225bcd18021c1a646fd8cff
Hugo Amorim <hugo@startupdesignninja.com>
2018-04-13 09:52:25 -0300

    Final Landing Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :brands
   resources :influencers
-  root to: 'pages#home'
+  root to: 'pages#LandingPage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
