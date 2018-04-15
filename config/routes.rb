20e01b2d61708b10ab7b3f0f476c756072caad44
Nanook Bergmann <inw.bergmann@gmail.com>
2018-04-15 16:41:20 -0300

    tried to solve scss conflicts of landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :influencers
 
   resources :mailings, except: [:edit, :update, :destroy]
-  root to: 'pages#LandingPage'
+  root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
