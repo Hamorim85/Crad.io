4933b20e65d85b6690ad4e2e703f5c9671b4133c
Nanook Bergmann <inw.bergmann@gmail.com>
2018-04-17 14:57:14 -0300

    gave the mailer a nice layout

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -1,7 +1,7 @@
 class MailingsController < ApplicationController
   def index
     # All the former mailings that the brand created
-    @mailings = current_brand.mailings
+    @mailings = current_brand.mailings.order(created_at: :desc)
   end
 
   def show
@@ -33,7 +33,7 @@ class MailingsController < ApplicationController
       @influencers.each do |influencer|
         InfluencerMailer.inquiry(influencer).deliver_now
       end
-      redirect_to mailing_path(@mailing)
+      redirect_to mailings_path
     else
       puts @mailing.errors.full_messages
       render :new
