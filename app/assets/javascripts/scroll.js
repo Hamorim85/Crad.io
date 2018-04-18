40e56fb7ca5ea235672ec3df6fd71f5ee46e24cf
Nanook Bergmann <inw.bergmann@gmail.com>
2018-04-18 18:25:05 -0300

    deleted the javascript

--- app/assets/javascripts/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  alert('tets')
-  $('#animate').css('opacity', 0);
-
-  $('#animate').waypoint(function() {
-       $('#animate').addClass('slideInRight');
-    }
-  }, { offset: '100%'});
-});
